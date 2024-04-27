03eee5e58d4b6d7dd5cf4eacd03043c0bb4cda99
InalaSolvente <135399172+InalaSolvente@users.noreply.github.com>
2024-04-27 20:27:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,7 +41,6 @@ int is_equal(void* key1, void* key2){
   
 void insertMap(HashMap * map, char * key, void * value) {
   if(map==NULL || key==NULL) return;
-  Pair *par = createPair(key,value);
   unsigned long pos = hash(key,map->capacity);
   while(map->buckets[pos]!=NULL){
     pos++;
