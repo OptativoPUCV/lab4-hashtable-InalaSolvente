79b8af324a329754e89fb00e3a4a3db03cf5793a
InalaSolvente <135399172+InalaSolvente@users.noreply.github.com>
2024-04-27 21:00:04 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -123,7 +123,7 @@ Pair * firstMap(HashMap * map) {
 
 Pair *nextMap(HashMap *map) {
   if (map == NULL || map->current >= map->capacity - 1) return NULL;
-
   map->current++;
+  while(map->buckets[map->current] == NULL || map->buckets[map->current]->key == NULL) map->current++;
   return map->buckets[map->current];
 }
