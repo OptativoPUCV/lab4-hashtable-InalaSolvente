a51775710e27a63d08195a80ef5ba354733af186
InalaSolvente <135399172+InalaSolvente@users.noreply.github.com>
2024-04-27 20:42:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -115,7 +115,7 @@ Pair * firstMap(HashMap * map) {
   for(unsigned int i = 0; i < map->capacity; i++){
     if(map->buckets[i]!=NULL && map->buckets[i]->key != NULL) {
       map->current = i;
-      return map->buckets[i];
+      return map->buckets[i]->value;
     }
   }
   return NULL;
